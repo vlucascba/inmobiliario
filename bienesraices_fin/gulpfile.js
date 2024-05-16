a9fbc5ca571fef21a044d19a54eee596c8bdbd8d
Lucasvcba <lucas.e4e@gmail.com>
2024-05-16 14:59:37 -0300

    trabajo concluido

---
+++ bienesraices_fin/gulpfile.js
@@ -0,0 +1,78 @@
+import path from 'path'
+import fs from 'fs'
+import { glob } from 'glob'
+import { src, dest, watch, series } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import concat from 'gulp-concat'
+import terser from 'gulp-terser'
+import sharp from 'sharp'
+import rename from 'gulp-rename'
+
+const sass = gulpSass(dartSass)
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+}
+
+export function css( done ) {
+    src(paths.scss, {sourcemaps: true})
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError) )
+        .pipe( dest('./build/css', {sourcemaps: '.'}) );
+    done()
+}
+
+export function js( done ) {
+    src(paths.js)
+      .pipe(concat('bundle.js'))
+      .pipe(terser())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(dest('./build/js'))
+    done()
+}
+
+export async function imagenes(done) {
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    const images =  await glob('./src/img/**/*')
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
+    done();
+}
+
+function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+
+    if (extName.toLowerCase() === '.svg') {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        fs.copyFileSync(file, outputFile);
+    } else {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
+        const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`);
+        const options = { quality: 80 };
+
+        sharp(file).jpeg(options).toFile(outputFile);
+        sharp(file).webp(options).toFile(outputFileWebp);
+        sharp(file).avif().toFile(outputFileAvif);
+    }
+}
+
+export function dev() {
+    watch( paths.scss, css );
+    watch( paths.js, js );
+    watch('src/img/**/*.{png,jpg}', imagenes)
+}
+
+export default series( js, css, imagenes, dev )
